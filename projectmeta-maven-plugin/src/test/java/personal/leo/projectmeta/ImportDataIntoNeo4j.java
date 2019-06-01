7ea22379b921a6d9b6e5d219fe00def4cdc500d8
liaochao.lc <liaochao.lc@alibaba-inc.com>
2019-06-02 01:00:43 +0800

    增加单测

---
+++ projectmeta-maven-plugin/src/test/java/personal/leo/projectmeta/ImportDataIntoNeo4j.java
@@ -0,0 +1,14 @@
+package personal.leo.projectmeta;
+
+import org.junit.Test;
+
+/**
+ * @author 谦扬
+ * @date 2019-06-02
+ */
+public class ImportDataIntoNeo4j {
+    @Test
+    public void test0() {
+
+    }
+}
